13e58f9025819c00fc17bb7377d67036995733dd
James Montemagno <james.montemagno@gmail.com>
2019-02-22 16:56:56 -0800

    Add SocialItem and Bindable Layout!

---
+++ src/Hanselman/Models/SocialItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Hanselman.Helpers;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Hanselman.Models
+{
+    public class SocialItem
+    {
+        public SocialItem()
+        {
+            OpenUrlCommand = new Command(async () => await OpenSocialUrl());
+        }
+
+        public string Icon { get; set; }
+        public string Url { get; set; }
+
+        public ICommand OpenUrlCommand { get; }
+
+        async Task OpenSocialUrl()
+        {
+            if (Url.Contains("twitter"))
+            {
+                var launch = DependencyService.Get<ILaunchTwitter>();
+                if (launch?.OpenUserName("shanselman") ?? false)
+                    return;
+            }
+            await Browser.OpenAsync(Url);
+        }
+    }
+}
---
+++ src/Hanselman/ViewModels/AboutViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Hanselman.Models;
+using MvvmHelpers;
+
+namespace Hanselman.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public List<SocialItem> SocialItems { get; }
+        public AboutViewModel()
+        {
+            SocialItems = new List<SocialItem>
+            {
+                new SocialItem
+                {
+                    Icon = "twitter.png",
+                    Url = "https://www.twitter.com/shanselman"
+                },
+                new SocialItem
+                {
+                    Icon = "facebook.png",
+                    Url = "https://www.facebook.com/shanselman"
+                },
+                new SocialItem
+                {
+                    Icon = "instagram.png",
+                    Url = "https://www.instagram.com/shanselman"
+                }
+            };
+        }
+    }
+}
