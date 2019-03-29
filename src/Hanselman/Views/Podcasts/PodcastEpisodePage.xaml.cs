92a3ba18cb7392428dffe89bc7a5f82d0e537422
James Montemagno <james.montemagno@gmail.com>
2019-03-29 16:24:36 -0700

    Start of podcast episode details page.

---
+++ src/Hanselman/Views/Podcasts/PodcastEpisodePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hanselman.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Hanselman.Views.Podcasts
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PodcastEpisodePage : ContentPage
+    {
+        public PodcastEpisodePage(PodcastEpisode episode)
+        {
+            InitializeComponent();
+            BindingContext = episode;
+        }
+        public PodcastEpisodePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButtonClose_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
