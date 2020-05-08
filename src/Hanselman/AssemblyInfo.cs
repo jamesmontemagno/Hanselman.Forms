bad5c8974f80870cf8d7af755157a549842bed39
James Montemagno <james.montemagno@gmail.com>
2020-05-08 14:53:26 -0700

    Upgraded Nugets & Fixed Fonts!

--- src/Hanselman/AssemblyInfo.cs
+++ src/Hanselman/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 using Xamarin.Forms.Xaml;
 using Xamarin.Forms;
 
-[assembly: ExportFont("materialdesignicons-webfont.ttf")]
+[assembly: ExportFont("materialdesignicons-webfont.ttf", Alias = "MaterialDesignIcons")]
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
